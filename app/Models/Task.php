c9d12b6d04fdc22c76e1285965c8400c356aa3a3
M Abdulkarim <69695051+mabdulkarim@users.noreply.github.com>
2022-11-26 17:03:35 +0100

    database architecture

---
+++ app/Models/Task.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'client_id',
+        'product_id',
+        'title',
+        'description',
+        'deadline'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+}
